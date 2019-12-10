3e4b0ca0dc5dd64dc8a7f428e5c83fee77113c29
Peter <peter.hessey@hotmail.com>
2019-12-10 13:12:43 +0000

    Fixed ratings

--- ratings_generator.py
+++ ratings_generator.py
@@ -1,16 +1,30 @@
 import csv
 import random
-import string
 
 NUMBER_OF_BOOKS = 30
 
-with open('./dataset/user_profiles.csv', 'w') as my_csv_file:
+with open('./dataset/ratings.csv', 'w') as my_csv_file:
 
     csv_writer = csv.writer(my_csv_file)
-    csv_writer.writerow(['user_ID', 'username', 'password'])
-    valid_chars = string.ascii_lowercase
+    csv_writer.writerow(['user_ID', 'book_ID', 'book_rating'])
+    ratings = {}
+
     for user_ID in range(30):
-        username = ''.join(random.choice(valid_chars) for _ in range(6))
-        password = 'password'
-        data_entry = [str(user_ID), username, password] 
-        csv_writer.writerow(data_entry)
+        ratings_to_make = 5
+
+        while ratings_to_make != 0:
+            make_rating = True
+            new_book_ID = random.randint(0, 30)
+
+            if user_ID in ratings:
+                if new_book_ID in ratings[user_ID]:
+                    make_rating = False
+            else:
+                ratings[user_ID] = []
+
+            if make_rating:
+                ratings[user_ID].append(new_book_ID)
+                book_rating = random.randint(0, 5)
+                row = [str(user_ID), str(new_book_ID), str(book_rating)]
+                csv_writer.writerow(row)
+                ratings_to_make -= 1
